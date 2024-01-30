42552255f5c0de4d87353070909cb40c51e733a4
MatteoSanson <matteo.sanson@gmail.com>
2024-01-30 15:35:48 +0100

    feat: scaffolding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Template</title>
+    <title>Laravel Train Station</title>
     @vite('resources/js/app.js')
 </head>
+
 <body>
     <div class="container text-center">
-        <h1 class="py-3">Template Laravel</h1>
+        <h1 class="py-3">Train Station</h1>
     </div>
 </body>
+
 </html>
