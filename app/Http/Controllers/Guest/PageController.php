0ad042e4d2523f6c2f0e374e0c69f48a53447ab1
MatteoSanson <matteo.sanson@gmail.com>
2024-01-30 19:38:59 +0100

    feat: prova card

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class PageController extends Controller
 {
     public function index(){
-        return view('welcome');
+
+        $trains = Train::all();
+        // dd($trains);
+        return view('welcome', compact('trains'));
     }
 }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,69 @@
 </head>
 
 <body>
-    <div class="container text-center">
-        <h1 class="py-3">Train Station</h1>
-    </div>
+    <header>
+        <div class="container text-center">
+            <h1 class="py-3">Laravel Train Station</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="container mt-5">
+            <div class="row">
+                @foreach ($trains as $train)
+                    <div class="col-md-6 mb-4">
+                        <div class="card h-100">
+                            <div class="card-header">
+                                <h5 class="card-title text-right">Biglietto {{ $train['azienda'] }}</h5>
+                            </div>
+                            <div class="card-body d-flex flex-column">
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <p><strong>Partenza:</strong> {{ $train['stazione_partenza'] }}</p>
+                                    </div>
+                                    <div class="col-md-4 text-right">
+                                        <p><strong>Codice-Treno:</strong> {{ $train['codice_treno'] }}</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <p><strong>Arrivo:</strong> {{ $train['stazione_arrivo'] }}</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-9">
+                                        <p><strong>Data Partenza:</strong> {{ $train['giorno_partenza'] }}
+                                            <strong>ore:</strong> {{ $train['orario_partenza'] }}
+                                        </p>
+                                    </div>
+                                    <div class="col-md-3 text-right">
+                                        <p><strong>N carrozze:</strong> {{ $train['n_carrozze'] }}</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-9">
+                                        <p><strong>Data Arrivo:</strong> {{ $train['giorno_arrivo'] }}
+                                            <strong>ore:</strong> {{ $train['orario_arrivo'] }}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <p class="text-center"><strong>In Orario:</strong>
+                                            {{ $train['in_orario'] ? 'Si' : 'No' }}</p>
+                                    </div>
+                                    <div class="col-md-6 text-center">
+                                        <p><strong>Cancellato:</strong> {{ $train['cancellato'] ? 'Si' : 'No' }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </main>
+
 </body>
 
 </html>
