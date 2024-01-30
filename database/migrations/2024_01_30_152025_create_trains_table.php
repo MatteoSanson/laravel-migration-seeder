b647eb6fb24e0605edce36d7ce06724a9e1c2ab1
MatteoSanson <matteo.sanson@gmail.com>
2024-01-30 17:35:32 +0100

    feat: col table

--- database/migrations/2024_01_30_152025_create_trains_table.php
+++ database/migrations/2024_01_30_152025_create_trains_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda',30);
+            $table->string('stazione_partenza',30);
+            $table->string('stazione_arrivo',30);
+            $table->time('orario_partenza');
+            $table->date('giorno_partenza');
+            $table->time('orario_arrivo');
+            $table->date('giorno_arrivo');
+            $table->BigInteger('codice_treno')->unsigned()->nullable();
+            $table->TinyInteger('n_carrozze')->unsigned()->nullable();
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
             $table->timestamps();
         });
     }
