afde30fa13eb1240ccaa5eb6d910539460a96c7b
MatteoSanson <matteo.sanson@gmail.com>
2024-01-30 21:21:22 +0100

    feat: pagecontroller use carbon

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -6,15 +6,28 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Train;
 
-class PageController extends Controller
-{
-    public function index(){
+// class PageController extends Controller
+// {
+//     public function index(){
 
-        $trains = Train::all();
-        // dd($trains);
-        return view('welcome', compact('trains'));
-    }
-}
+//         $trains = Train::all();
+//         // dd($trains);
+//         return view('welcome', compact('trains'));
+//     }
+// }
 
 
 // php artisan make:migration create_trains_table
+
+use Illuminate\Support\Carbon;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $currentDate = Carbon::now()->toDateString();
+        $trains = Train::whereDate('giorno_partenza', '=', $currentDate)->get();
+        
+        return view('welcome', compact('trains'));
+    }
+}
